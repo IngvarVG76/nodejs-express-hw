46489bb02bcf1b6cea63d1a533fecbaa1c7fe524
Igor Grybanov <igor.gribanov@gmail.com>
2023-09-20 23:51:29 +0300

    Task done

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ import cors from 'cors';
 
 import "dotenv/config";
 
-
-
+import authRouter from "./routes/api/auth-router.js";
 import contactsRouter from "./routes/api/contacts-router.js"
 
 const app = express();
@@ -15,6 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRouter);
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
---
+++ models/User.js
@@ -0,0 +1,55 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+
+import { handleSaveError, runValidateAtUpdate } from "./hooks.js";
+
+const emailRegexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const subscriptionTypes = ["starter", "pro", "business"];
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        match: emailRegexp,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: true,
+    },
+    subscription: {
+        type: String,
+        enum: subscriptionTypes,
+        default: "starter"
+    },
+    token: {
+        type: String,
+    }
+}, { versionKey: false, timestamps: true })
+
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", runValidateAtUpdate);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+export const userSignupSchema = Joi.object({
+    username: Joi.string().required(),
+    email: Joi.string().pattern(emailRegexp).required(),
+    password: Joi.string().min(6).required(),
+    subscription: Joi.string().valid(...subscriptionTypes).required(),
+})
+
+export const userSigninSchema = Joi.object({
+    email: Joi.string().pattern(emailRegexp).required(),
+    password: Joi.string().min(6).required(),
+})
+
+const User = model("user", userSchema);
+
+export default User;
